f18e72be5e18c105f86d3352aa06457b1fd6918d
morgan_p <morgan.prieur@tetradis.com>
2024-02-14 16:02:15 +0100

    Age validator ok

--- api/users/serializers/userSerializer.py
+++ api/users/serializers/userSerializer.py
@@ -1,17 +1,32 @@
 
 from django.contrib.auth.models import User 
-# utils 
-from rest_framework import serializers 
-
 from users.models import UserProfile 
+
 # utils 
 from rest_framework import serializers 
+from django.contrib.auth.hashers import make_password 
+from rest_framework.validators import UniqueValidator 
 # import re 
 # from datetime import datetime, timedelta 
-from django.contrib.auth.hashers import make_password 
+
+
+def minimum_age(value): 
+    """ Validator to not create a User instance if the age < 15. 
+
+        Args:
+            value (integer): The declared age of the user. 
+
+        Raises:
+            serializers.ValidationError: The cause of the error. 
+    """ 
+    if value < 15:
+        raise serializers.ValidationError('Vous devez être âgé d\'au moins 15 ans.') 
 
 
 class UserSerializer(serializers.ModelSerializer): 
+    """ Class to serialize/deserialize a User instance, 
+        from the User model. 
+    """ 
     class Meta: 
         model = User 
         fields = ( 
@@ -23,7 +38,12 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class UserPofileSerializer(serializers.ModelSerializer): 
+    """ Class to serialize/deserialize a UserProfile instance, 
+        from the UserProfile model. 
 
+        Returns:
+            UserProlife: A UserProfile or some UserProfile instances. 
+    """ 
     user = UserSerializer() 
 
     class Meta: 
@@ -37,39 +57,25 @@ class UserPofileSerializer(serializers.ModelSerializer):
         ) 
 
     def create(self, validated_data): 
-        print('validated_data UPS41 : ', validated_data) 
-        if 'user' in validated_data.keys(): 
-            print('user yes') 
-            user_data = validated_data.pop('user') 
-            user_data['password'] = make_password(user_data['password']) 
-            # get_user = User.objects.get(username=user_data['username']) 
-            # print('get_user : ', get_user) 
-            # if not get_user: 
-            new_user = User.objects.create(**user_data) 
-            # last_user = User.objects.last() 
-            get_user = User.objects.last() 
-            print('last_user : ', get_user) 
+        # print('validated_data UPS41 : ', validated_data) 
+        if validated_data['age'] > 15: 
+            # print('if > 15') 
+            if 'user' in validated_data.keys(): 
+                # print('user yes') 
+                user_data = validated_data.pop('user') 
+                user_data['password'] = make_password(user_data['password']) 
 
-            return UserProfile.objects.create( 
-                user = get_user, 
-                **validated_data, 
-            ) 
+                new_user = User.objects.create(**user_data) 
+                get_user = User.objects.last() 
+                # print('last_user : ', get_user) 
 
-
-# class UserSerializer(serializers.ModelSerializer): 
-
-#     class Meta: 
-#         model = User 
-#         fields = ( 
-#             'id', 
-#             'username', 
-#             'password', 
-#             'groups', 
-#         ) 
-
-#     def create(self, validated_data): 
-#         if 'user' in validated_data.keys(): 
-#             validated_user_data = validated_data['user'] 
-#             validated_user_data['password'] = make_password(validated_user_data['password']) 
-#             return User.objects.create(**validated_user_data) 
+                return UserProfile.objects.create( 
+                    user = get_user, 
+                    **validated_data, 
+                ) 
+        else:  
+            # print('else') 
+            # print(validated_data['age']) 
+            age = minimum_age(validated_data['age']) 
+            return False 
 
--- api/users/views.py
+++ api/users/views.py
@@ -1,6 +1,5 @@
 
-# from django.shortcuts import render 
-from django.contrib.auth.models import User  # , Group 
+from django.contrib.auth.models import User 
 from users.models import UserProfile  # Contributor, 
 from users.serializers import ( 
     UserSerializer, 
@@ -8,16 +7,10 @@ from users.serializers import (
 ) 
 # from django_filters import rest_framework as filters 
 
-from rest_framework.permissions import IsAuthenticated 
-# from uthdemo.permissions import ( 
-#     IsAdminAuthenticated, 
-#     IsEditorGroup, 
-#     IsRecorderGroup, 
-#     IsReaderGroup 
-# ) 
 from rest_framework import generics, viewsets 
 from rest_framework.generics import CreateAPIView 
 from rest_framework.parsers import JSONParser 
+from rest_framework.permissions import IsAuthenticated 
 from rest_framework.response import Response 
 from rest_framework.views import APIView 
 
@@ -42,21 +35,9 @@ class UserViewSet(viewsets.ModelViewSet):
     # permission_classes = [permissions.IsAuthenticated] 
     # permission_classes = [IsAdminAuthenticated, ] 
 
-    # def post(self, request):
-    #     serializer = UserSerializer(data=request.data) 
-    #     if data['age'] < 15: 
-    #         return Response(status=status.HTTP_400_BAD_REQUEST)  # *** 
-    #     if serializer.is_valid(): 
-    #         serializer.save() 
-    #         return Response( 
-    #             serializer.data, status=status.HTTP_201_CREATED)
-    #     return Response( 
-    #         serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 # class SignupView(APIView): 
 class SignupView(CreateAPIView): 
-    # permission_classes = [IsAdminAuthenticated,]  # IsManagerGroup| 
     # serializer_class = UserPofileSerializer 
     # parser_classes = [JSONParser] 
 
@@ -73,50 +54,14 @@ class SignupView(CreateAPIView):
                     400 with the error if the request has not been completely executed. 
         """ 
         data = JSONParser().parse(request) 
-        print('data : ', data) 
-        # Save the data to the serializer for validating and saving them into the DB 
+        # print('data : ', data) 
+        # Save the data to the serializer for validating and saving them into the DB. 
         serializer = UserPofileSerializer(data=data) 
         if serializer.is_valid(): 
-            print('is_valid yes') 
-
+            # print('is_valid yes') 
             serializer.save() 
             return Response(serializer.data, status=201) 
         else: 
-            print('is_valid no') 
+            # print('is_valid no') 
             return Response(serializer.errors, status=400) 
-
-    # def post(self, request, format=None): 
-    #     """ 
-    #     """ 
-    #     data = JSONParser().parse(request) 
-    #     print('data : ', data) 
-    #     print('user : ', data['user_profile']['user']) 
-    #     print('age : ', data['user_profile']['age']) 
-    #     print('can_be_contacted : ', data['user_profile']['can_be_contacted']) 
-    #     # Send the data to the serializer for validating 
-    #     # and saving them in the DB 
-    #     # serializer = InstallationSerializer(data=data_installation) 
-    #     serializer = UserPofileSerializer(data=data) 
-    #     if serializer.is_valid(): 
-    #         print('is_valid') 
-    #         serializer.save()
-    #         return Response(serializer.data, status=201) 
-    #     else: 
-    #         print('no') 
-    #         return Response(serializer.errors, status=400) 
-
-
-
-# class NewClientView(CreateAPIView): 
-    
-#     # Authentication required 
-#     permission_classes = [IsAdminAuthenticated,]  # IsManagerGroup| 
-#     serializer_class = ClientSerializer 
-#     queryset = Client.objects.all() 
-
-
-
-# class BlogPostViewSet(viewsets.ModelViewSet):
-#     queryset = BlogPost.objects.all()
-#     serializer_class = BlogPostSerializer
 
