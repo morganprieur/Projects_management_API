42f78141f6a2a187570ea24058546f1ad8f3095c
morgan_p <morgan.prieur@tetradis.com>
2024-02-16 14:14:11 +0100

    WIP Get the contributors of the projects

--- api/softdesk/views.py
+++ api/softdesk/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User 
 from softdesk.models import Project 
 from softdesk.serializers import ProjectSerializer 
+from users.models import Contributor 
 from users.serializers import UserSerializer, ContributorSerializer 
 # from users.views import UserViewSet 
 
@@ -12,13 +13,110 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response 
 from rest_framework.views import APIView 
 
+from django.db.models import Q 
 
-class ProjectViewSet(viewsets.ModelViewSet): 
-    queryset = Project.objects.all() 
-    serializer_class = ProjectSerializer 
+
+# class ProjectViewSet(viewsets.ModelViewSet): 
+class ProjectView(APIView): 
+    # queryset = Project.objects.all() 
+    # serializer_class = ProjectSerializer 
     permission_classes = [IsAuthenticated] 
-    model = Project 
+    # model = Project 
 
+    def get(self, request): 
+        projects = [] 
+        contributors = [] 
+
+        projects_only = Project.objects.all() 
+        for proj in projects_only: 
+            # song = Song.objects.get(id='<the-song-id>')
+            # users_that_like_this_song = song.users_that_like_me.all()
+            # contribs = proj.contributors.all() 
+            # users_that_like_this_song.filter(name='Jon')
+            contribs = Contributor.objects.filter( 
+                project=proj 
+            ) 
+            print('contribs : ', contribs) 
+            print(proj.id) 
+            print(proj.author) 
+            print(proj.name) 
+            print(proj.description) 
+            print(proj.type)  
+            # for contrib in contribs: 
+            for contrib in contribs: 
+                print(contrib.id)  
+                print(contrib.project)  
+                print(contrib.user) 
+                # print('dir(proj) : ', dir(proj)) 
+                # contrib_serializer = ContributorSerializer( 
+                #     contrib, context={ 
+                #         'id': contrib.id, 
+                #         'project': contrib.project, 
+                #         'user': contrib.user, 
+                #     }
+                # ) 
+                # contributors.append(contrib_serializer.data) 
+                contributors.append(contrib) 
+                # print('contributors : ', contributors) 
+            serializer = ProjectSerializer( 
+                proj, context={ 
+                    'id': proj.id, 
+                    'author': proj.author, 
+                    'name': proj.name, 
+                    'description': proj.description, 
+                    'type': proj.type, 
+                    'contributors': ContributorSerializer( 
+                        contributors, context={ 
+                            'id': contrib.id, 
+                            'project': contrib.project, 
+                            'user': contrib.user, 
+                        }, 
+                    ) , 
+                    # { 
+                    #     'id': contrib.id, 
+                    #     'project': contrib.project, 
+                    #     'user': contrib.user, 
+                    # }, 
+                    'created_time': proj.created_time, 
+                }, 
+                # contrib_serializer, context={ 
+                #     'id': contrib.id, 
+                #     'project': contrib.project, 
+                #     'user': contrib.user, 
+                # }
+            ) 
+            # print('serializer.initial_data : ', serializer.initial_data)
+            print('serializer.data : ', serializer.data)
+            projects.append(serializer.data) 
+        return Response(projects) 
+
+    # def get(self, request): 
+
+    #     locations = [] 
+
+    #     # Get the entities 
+    #     user = User.objects.get(id=request.user.id) 
+    #     client_user = Client_profile.objects.get( 
+    #         client_user=user 
+    #     ) 
+    #     beiList = Bei.objects.filter( 
+    #         client=client_user.client 
+    #     ) 
+
+    #     # Loop into the registered beis 
+    #     for registered_bei in beiList: 
+    #         install = Installation.objects.get(bei=registered_bei) 
+    #         # Send the installation for the current bei and client 
+    #         serializer = LocationSerializer( 
+    #             install, context={ 
+    #                 'id': install.id, 
+    #                 'bei': install.bei.serial_number, 
+    #                 'client': install.bei.client.id 
+    #             } 
+    #         ) 
+    #         locations.append(serializer.data) 
+
+    #     return Response(locations) 
 
 class CreateProjectView(APIView): 
     queryset = Project.objects.all() 
@@ -45,9 +143,6 @@ class CreateProjectView(APIView):
             print('serializer.validated_data : ', serializer.validated_data) 
             serializer.save() 
             print('serializer data : ', serializer.data) 
-            return Response(serializer.data, status=200) 
+            return Response(serializer.data, status=201) 
         return Response(serializer.errors, status=400) 
-
-
-
 
--- api/users/serializers/contributorSerializer.py
+++ api/users/serializers/contributorSerializer.py
@@ -1,6 +1,7 @@
 
 from django.contrib.auth.models import User 
 from users.models import (Contributor, UserProfile) 
+from users.serializers import UserSerializer 
 
 # utils 
 from rest_framework import serializers 
@@ -10,37 +11,38 @@ from rest_framework.validators import UniqueValidator
 # from datetime import datetime, timedelta 
 
 
-def minimum_age(value): 
-    """ Validator to not create a User instance if the age < 15. 
+# def minimum_age(value): 
+#     """ Validator to not create a User instance if the age < 15. 
 
-        Args:
-            value (integer): The declared age of the user. 
+#         Args:
+#             value (integer): The declared age of the user. 
 
-        Raises:
-            serializers.ValidationError: The cause of the error. 
-    """ 
-    if value < 15:
-        raise serializers.ValidationError('Vous devez être âgé d\'au moins 15 ans.') 
+#         Raises:
+#             serializers.ValidationError: The cause of the error. 
+#     """ 
+#     if value < 15:
+#         raise serializers.ValidationError('Vous devez être âgé d\'au moins 15 ans.') 
 
 
-class UserSerializer(serializers.ModelSerializer): 
-    """ Class to serialize/deserialize a User instance, 
-        from the User model. 
-    """ 
-    class Meta: 
-        model = User 
-        fields = ( 
-            'id', 
-            'username', 
-            'password', 
-            'groups', 
-        ) 
+# class UserSerializer(serializers.ModelSerializer): 
+#     """ Class to serialize/deserialize a User instance, 
+#         from the User model. 
+#     """ 
+#     class Meta: 
+#         model = User 
+#         fields = ( 
+#             'id', 
+#             'username', 
+#             'password', 
+#             'groups', 
+#         ) 
 
 
 class ContributorSerializer(serializers.ModelSerializer): 
     """ Class to serialize/deserialize a Contributor instance, 
         from the Contributor model. 
     """ 
+    user = UserSerializer() 
     class Meta: 
         model = Contributor 
         fields = ( 
@@ -51,68 +53,55 @@ class ContributorSerializer(serializers.ModelSerializer):
         ) 
 
 
-class UserPofileSerializer(serializers.ModelSerializer): 
-    """ Class to serialize/deserialize a UserProfile instance, 
-        from the UserProfile model. 
+# class UserPofileSerializer(serializers.ModelSerializer): 
+#     """ Class to serialize/deserialize a UserProfile instance, 
+#         from the UserProfile model. 
 
-        Returns:
-            UserProlife: A UserProfile or some UserProfile instances. 
-    """ 
-    user = UserSerializer() 
+#         Returns:
+#             UserProlife: A UserProfile or some UserProfile instances. 
+#     """ 
+#     user = UserSerializer() 
 
-    class Meta: 
-        model = UserProfile 
-        fields = ( 
-            'id', 
-            'user', 
-            'age', 
-            'can_be_contacted', 
-            'data_can_be_shared', 
-            'created_time', 
-        ) 
+#     class Meta: 
+#         model = UserProfile 
+#         fields = ( 
+#             'id', 
+#             'user', 
+#             'age', 
+#             'can_be_contacted', 
+#             'data_can_be_shared', 
+#             'created_time', 
+#         ) 
 
-    def create(self, instance, validated_data): 
-        # print('create instance UPS67 : ', instance) 
-        # print('create validated_data UPS68 : ', validated_data) 
-        if 'age' in validated_data.keys(): 
-            if validated_data['age'] > 15: 
-                # print('if age > 15') 
-                if 'user' in validated_data.keys(): 
-                    # print('user yes') 
-                    user_data = validated_data.pop('user') 
-                    user_data['password'] = make_password(user_data['password']) 
+#     def create(self, instance, validated_data): 
+#         # print('create instance UPS67 : ', instance) 
+#         # print('create validated_data UPS68 : ', validated_data) 
+#         if 'age' in validated_data.keys(): 
+#             if validated_data['age'] > 15: 
+#                 # print('if age > 15') 
+#                 if 'user' in validated_data.keys(): 
+#                     # print('user yes') 
+#                     user_data = validated_data.pop('user') 
+#                     user_data['password'] = make_password(user_data['password']) 
 
-                    new_user = User.objects.create(**user_data) 
-                    get_user = User.objects.last() 
-                    # print('last_user : ', get_user) 
+#                     new_user = User.objects.create(**user_data) 
+#                     get_user = User.objects.last() 
+#                     # print('last_user : ', get_user) 
 
-                    return UserProfile.objects.create( 
-                        user = get_user, 
-                        **validated_data, 
-                    ) 
-            else:  
-                # print('else') 
-                # print(validated_data['age']) 
-                age = minimum_age(validated_data['age']) 
-                return False 
-        else: 
-            profile = profile.update( 
-                **validated_data, 
-            ) 
-            profile.save() 
-            print('profile : ', profile) 
-            return profile 
-
-
-        
-
-    # def update(self, validated_data): 
-    #     print('update validated_data UPS84 : ', validated_data) 
-    #     # updated_profile = {} 
-    #     # updated_profile['can_be_contacted'] = validated_data['can_be_contacted'] 
-    #     # updated_profile['data_can_be_shared'] = validated_data['data_can_be_shared'] 
-    #     # return UserProfile.objects.update( 
-    #     #     **updated_profile, 
-    #     # ) 
-
+#                     return UserProfile.objects.create( 
+#                         user = get_user, 
+#                         **validated_data, 
+#                     ) 
+#             else:  
+#                 # print('else') 
+#                 # print(validated_data['age']) 
+#                 age = minimum_age(validated_data['age']) 
+#                 return False 
+#         else: 
+#             profile = profile.update( 
+#                 **validated_data, 
+#             ) 
+#             profile.save() 
+#             print('profile : ', profile) 
+#             return profile 
 
