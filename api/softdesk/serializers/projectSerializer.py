4f8ae7e5bc3c3df0eb0101de0db0e8bb96c97b0c
Morgan Prieur <79154432+morganprieur@users.noreply.github.com>
2024-04-22 16:51:38 +0200

    Update projectSerializer.py

--- api/softdesk/serializers/projectSerializer.py
+++ api/softdesk/serializers/projectSerializer.py
@@ -5,8 +5,6 @@ from softdesk.models import Project
 from users.serializers import UserSerializer 
 # utils 
 from rest_framework import serializers 
-# import re 
-# from datetime import datetime, timedelta 
 
 
 class ProjectSerializer(serializers.ModelSerializer): 
